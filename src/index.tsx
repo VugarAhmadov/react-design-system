990656fe4f4bd472f33cbc1605745bd72212354f
Jean-Christophe Pires <jeanchristophe.pires55@gmail.com>
2020-04-21 09:31:21 +0200

    remove import .less

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import '../src/default.style.less'
+
 
 export {default as RecherchePatient} from './components/organisms/Jumpers/RecherchePatient'
 export { default as Button } from './components/atomics/buttons'
